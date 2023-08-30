8cae30b7c0be84b70f076c4da998a92c960b9c5e
daniil <daniilzhilyaev@gmail.com>
2023-08-30 13:53:06 +0300

    add task 46

--- 001_Task46/Program.cs
+++ 001_Task46/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача 46:** Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// m = 3, n = 4.
+
+// 1 4 8 19
+
+// 5 -2 33 -2
+
+// 77 3 8 1
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+
+
+// Создание массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+int[,] res = new int[m,n];
+for(int i = 0; i < m; i++){
+for(int j = 0; j < n; j++){
+res[i,j] = new Random().Next(minValue, maxValue + 1);
+}
+}
+return res;
+}
+// Печать массива
+void PrintArray(int[,] array){
+for(int i = 0; i < array.GetLength(0); i++){
+for(int j = 0; j < array.GetLength(1); j++){
+Console.Write($"{array[i,j]} ");
+}
+Console.WriteLine();
+}
+}
