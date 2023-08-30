32d3693d1fd1f10310dc46dd5a970f52eb4024a8
daniil <daniilzhilyaev@gmail.com>
2023-08-30 15:20:31 +0300

    add HW task 47

---
+++ 005_HW_Task47/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+
+
+// Создание массива
+double[,] GetArray(int m, int n)
+{
+    double[,] res = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().NextDouble();
+        }
+    }
+    return res;
+}
+// Печать массива
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
