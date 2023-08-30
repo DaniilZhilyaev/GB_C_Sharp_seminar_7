ee50c7f45cddf513118558fcf5435a7ef91df47c
daniil <daniilzhilyaev@gmail.com>
2023-08-30 13:54:30 +0300

    add task 46

--- 001_Task46/Program.cs
+++ 001_Task46/Program.cs
@@ -22,20 +22,25 @@ PrintArray(array);
 // Создание массива
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-int[,] res = new int[m,n];
-for(int i = 0; i < m; i++){
-for(int j = 0; j < n; j++){
-res[i,j] = new Random().Next(minValue, maxValue + 1);
-}
-}
-return res;
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
 }
 // Печать массива
-void PrintArray(int[,] array){
-for(int i = 0; i < array.GetLength(0); i++){
-for(int j = 0; j < array.GetLength(1); j++){
-Console.Write($"{array[i,j]} ");
-}
-Console.WriteLine();
-}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
 }
